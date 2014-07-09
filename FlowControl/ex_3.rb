3d2f3c7ad6d12afb909c8a2fa7e386f35d2ec6c0
DaddyDanielT <artsppdaniel@gmail.com>
2014-07-09 23:33:03 +0800

    Modified Tab size=2

--- FlowControl/ex_3.rb
+++ FlowControl/ex_3.rb
@@ -1,12 +1,12 @@
 def numberRange(n)
   if (n<0)
-  	"Please don't give me the negative number."  
+    "Please don't give me the negative number."  
   elsif( n>=0 && n <50)
-  	"#{n} is between 0~50"
+    "#{n} is between 0~50"
   elsif( n>50 && n<=100)
-  	"#{n} is between 51~100"
+    "#{n} is between 51~100"
   else (n>100)
-  	"#{n} is above 100"
+    "#{n} is above 100"
   end
 end
 
